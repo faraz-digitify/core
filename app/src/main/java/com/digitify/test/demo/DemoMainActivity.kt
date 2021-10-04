f760cf736fd57684921dd729474a1dca980c9cee
Faheem Riaz <faheem@digitify.com>
2021-10-04 21:31:38 +0500

    -Remove utils

--- app/src/main/java/com/digitify/test/demo/DemoMainActivity.kt
+++ app/src/main/java/com/digitify/test/demo/DemoMainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.digitify.test.R
 import dagger.hilt.android.AndroidEntryPoint
-import com.yap.base.utills.BIO_METRIC_SCREEN_TYPE
 
 @AndroidEntryPoint
 class DemoMainActivity : AppCompatActivity() {
